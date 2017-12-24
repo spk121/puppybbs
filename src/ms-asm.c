9b69a4e72f64ce5953f982a7a500d89b3f354813
Mike Gran <spk121@yahoo.com>
2017-12-23 23:50:00 -0800

    Guess at MS-DOS time/date and crc functions

--- src/ms-asm.c
+++ src/ms-asm.c
@@ -1,39 +1,51 @@
+#include <time.h>
 #include "ms-asm.h"
 
+/* MS-DOS 16-bit time appears to have been
+   Bits 0-4: seconds divided by 2
+   Bits 5-10: Minutes 0 to 59
+   Bits 11-15: Hour 0 to 23
+*/
 uint16_t gtime()
 {
-	unsigned ret = 0;
-	printf("in dummy func gtime() returning %u\n", ret);
-	return ret;
+	time_t now;
+	time(&now);
+	struct tm *tt = localtime(&now);
+	return (tt->tm_sec / 2 | (tt->tm_min << 5) | (tt->tm_hour << 11));
 }
 
+/* MS-DOS 16-bit date appears to have been
+  Bits 0-4 day of month 1-31
+  Bits 5-8 Month with 1 = January, 2 = February, etc
+  Bits 9-15 Years - 1980
+  */
 uint16_t gdate()
 {
-	unsigned ret = 0;
-	printf("in dummy func gdate() returning %u\n", ret);
-	return ret;
+	time_t now;
+	time(&now);
+	struct tm *tt = localtime(&now);
+	return (tt->tm_mday | (tt->tm_mon + 1) << 5 | ((tt->tm_year - 80) << 9));
 }
 
+uint16_t _crc;
 void clrcrc()
 {
-	printf("in dummy function clrcrc()\n");
+	_crc = 7;
 }
 
 void updcrc(char c)
 {
-	printf("in dummy function updcrc(%d)\n", c);
+	_crc = _crc * 37 + c;
 }
 
 unsigned fincrc()
 {
-	unsigned ret = 0;
-	printf("in dummy func fincrc() returning %u\n", ret);
-	return ret;
+	return _crc;
 }
 
 int chkcrc()
 {
-	int ret = 0;
+	int ret = 1;
 	printf("in dummy func chkcrc() returning %d\n", ret);
 	return ret;
 }
